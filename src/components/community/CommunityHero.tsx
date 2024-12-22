6ab6ab10787fd26a06286db16812067670e34608
Mayowa <mayorfalomo@gmail.com>
2024-12-22 13:01:27 +0100

    Update CommunityHero.tsx

--- src/components/community/CommunityHero.tsx
+++ src/components/community/CommunityHero.tsx
@@ -10,7 +10,7 @@ const CommunityHero: React.FC = () => {
         header="Community"
         subHeader="Empowering Change, One Life at Time."
         subText="Join a community committed to elevating your potential, driving your growth, and inspiring positive change. Together, we can make an Impact."
-        mainConStyle="h-[60vh] max-[600px]:h-[70vh] max-[480px]:h-[80vh] w-[80%] mx-auto"
+        mainConStyle="h-[60vh] max-[600px]:h-[70vh] max-[480px]:h-[60vh] w-[80%] mx-auto"
         subConHeaderStyle="scroll-m-20 tracking-tight text-[20px] !font-extrabold"
         subConStyle="max-w-[520px] text-center mt-1"
         headerStyle="text-[35px]"
