c8b26ee8f2ee02a0b2edc367f3d6d1837b79da59
mayor falomo <mayorfalomo@gmail.com>
2024-12-22 11:18:23 +0100

    added star

--- src/components/homepage/OurFoundation.tsx
+++ src/components/homepage/OurFoundation.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import fdnImg from "@/assets/foundation.png";
-import clans from "@/assets/clan.png";
-import Texts from "../Texts";
-import BlurText from "../animation/BlurText";
-import { useInView } from "react-intersection-observer";
+import React from 'react';
+import fdnImg from '@/assets/foundation.png';
+import clans from '@/assets/clan.png';
+import Texts from '../Texts';
+import BlurText from '../animation/BlurText';
+import { useInView } from 'react-intersection-observer';
+import Star from '@/assets/Star.png';
 
 const OurFoundation: React.FC = () => {
   const [refOne, inViewOne] = useInView({
@@ -12,8 +13,8 @@ const OurFoundation: React.FC = () => {
   });
 
   return (
-    <div className="h-[60vh] max-[480px]:h-[100vh] max-[1200px]:min-h-screen flex flex-col justify-center items-center bg-greyish">
-      <div className="px-4 py-[30px] min-w-[400px] max-[500px]:min-w-[90%] min-[1800px]:w-[1500px] max-[1800px]:w-[90%] max-[1200px]:w-[60%] mx-auto h-[60%] max-[1200px]:h-auto bg-white rounded-[15px]">
+    <div className="relative h-[60vh] max-[480px]:h-[100vh] max-[1200px]:min-h-screen flex flex-col justify-center items-center bg-greyish">
+      <div className="relative z-[9] px-4 py-[30px] min-w-[400px] max-[500px]:min-w-[90%] min-[1800px]:w-[1500px] max-[1800px]:w-[90%] max-[1200px]:w-[60%] mx-auto h-[60%] max-[1200px]:h-auto bg-white rounded-[15px]">
         <div
           ref={refOne}
           className="flex max-[1200px]:flex-col max-[1200px]:items-center justify-between gap-4 max-[1200px]:gap-[30px] mx-auto w-[95%] h-[100%]"
@@ -26,21 +27,24 @@ const OurFoundation: React.FC = () => {
               <img src={fdnImg} alt="img" />
             </div>
           </div>
-          <div className="w-[200px] bg-[#D9D9D9] p-3 rounded-[10px]">
+          <div className="w-[200px]  bg-[#D9D9D9] p-3 rounded-[10px]">
             <img src={clans} alt="img" />
           </div>
           <div className="flex flex-col items-start justify-between gap-[10px] max-w-[400px]">
             <Texts>
-              We believe every idea is a gift from God, with the potential to
-              grow and make a lasting impact.{" "}
+              We believe every idea is a gift from God, with the potential to grow and
+              make a lasting impact.{' '}
             </Texts>
             <Texts>
-              Rooted in faith, trust, and collaboration, we are dedicated to
-              helping individuals and ministries fulfill their God-given
-              purpose. Together, we turn vision into reality, all for His glory.{" "}
+              Rooted in faith, trust, and collaboration, we are dedicated to helping
+              individuals and ministries fulfill their God-given purpose. Together, we
+              turn vision into reality, all for His glory.{' '}
             </Texts>
           </div>
         </div>
+      </div>
+      <div className="absolute top-0 left-0 w-[150px] z-1">
+        <img src={Star} alt="img" />
       </div>
     </div>
   );
