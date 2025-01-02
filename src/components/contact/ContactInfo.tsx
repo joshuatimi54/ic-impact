1a631d3858923fbba04d3d2a4a48f2e80fb10421
Mayowa <mayorfalomo@gmail.com>
2025-01-02 19:52:13 +0100

    Update ContactInfo.tsx

--- src/components/contact/ContactInfo.tsx
+++ src/components/contact/ContactInfo.tsx
@@ -7,19 +7,15 @@ const ContactInfo = () => {
     <div>
       <div className="min-[1800px]:max-w-[1200px] max-[1800px]:max-w-[1100px] max-[1800px]:w-[90%] mx-auto min-[900px]:flex max-[900px]:flex-col min-[900px]:items-end max-[900px]:items-center justify-between gap-[30px] my-[50px]">
         <div className="flex flex-col max-[900px]:mx-auto gap-4 max-w-[350px]">
-          <Texts> Location/Address: Find us at No 4 FUTA South Gate Junction...</Texts>
           <Texts>
             <strong>For more direct or urgent communication</strong>, contact the email
             address below:
           </Texts>
           <Texts>
-            {/* <span>
-              <strong>Call us at: +234 123 456 7890</strong>{' '}
-            </span> */}
             <span>
               email us at:{' '}
-              <a href="mailto:support@ic-impact@gmail.com" rel="noopener noreferrer">
-                <strong>support@icimpact@gmail.online</strong>{' '}
+              <a href="mailto:support@icimpact.online" rel="noopener noreferrer">
+                <strong>support@icimpact.online</strong>{' '}
               </a>{' '}
             </span>
           </Texts>
