65465de5d38d57247de0b3ade77494d63a6909ee
Joshua Adesoba <joshuatimi54@gmail.com>
2025-05-31 22:21:15 +0100

    Cohort 2 live

--- src/components/signup/SignUpHero.tsx
+++ src/components/signup/SignUpHero.tsx
@@ -2,7 +2,7 @@ import SignUpHeroImage from '@/assets/signUpHero.png';
 
 const SignUpHero = () => {
   return (
-    <div className="min-h-[60vh]  w-full flex flex-col justify-center items-center ">
+    <div className="min-h-[30vh]  w-full flex flex-col justify-center items-center ">
       <div className="h-[100px] w-full"></div>
       <div className="max-w-[800px]">
         <img src={SignUpHeroImage} alt="img" />
